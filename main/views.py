d49aa6c31385602fd3c16c16ac01c669077576d1
Nikiforov Pavel <pavelnikif@gmail.com>
2021-03-30 14:03:08 +0300

    n turn_on_block, show user name

--- main/views.py
+++ main/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+class IndexView(TemplateView):
+    """Main page"""
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['turn_on_block'] = True
+        context['content'] = 'Content of index page'
+        context['title'] = 'Index page'
+        context['user'] = getattr(self.request, 'user', 'Unknown')
+
+        return context
+
