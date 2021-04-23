cf7db90dfd18918dc622ad602fd1833a5b99fc73
Nikiforov Pavel <pavelnikif@gmail.com>
2021-04-23 12:52:04 +0300

    test pytest for get pages

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class GetPagesTestCase(TestCase):
+    fixtures = ["category.json", "price.json", "product.json", "permission.json", "group.json",
+                "user.json", "profile.json", "seller.json", "tag.json"]
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail(self):
+        response = self.client.get('/products/13/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_list(self):
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_detail(self):
+        response = self.client.get('/profile/1/')
+        self.assertEqual(response.status_code, 200)
